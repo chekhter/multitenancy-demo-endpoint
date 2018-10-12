bda7176a2696bc1b94c230e3f45796bc77911f62
Kirill Chekhter <kirill.chekhter@sap.com>
2018-10-12 17:46:07 +0200

    Add scope check to the app

--- app/router/routes/testEndpoint.js
+++ app/router/routes/testEndpoint.js
@@ -1,10 +1,32 @@
 /*eslint no-shadow: 0*/
 "use strict";
 
-module.exports = function() {
+module.exports = testEndpoint;
+
+
+function hasScope(scopeName) {
+    return function (request, response, next) {
+        if (!request.authInfo) {
+            console.log('Error creating SecurityContext');
+            response.status(403).end('Forbidden');
+            return;
+        }
+
+        console.log(`Verifying scope (${scopeName}) in jwt`);
+        if (!request.authInfo.checkLocalScope(scopeName)) {
+            console.log(`Missing the expected scope (${scopeName})`);
+            response.status(403).end('Forbidden');
+        } else {
+            next();
+        }
+    };
+}
+
+
+function testEndpoint() {
 	var app = require("express").Router();
 	
-	app.get('/', function(req, res) {
+	app.get('/', hasScope("SCHEDULER"), function(req, res) {
 		console.log("Endpoint triggered via jobscheduler");
 		res.status(201).json({success: true});});
 	return app;
